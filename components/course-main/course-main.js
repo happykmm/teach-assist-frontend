ecbb8ba9b808070e4a6220bd8df97112ee1b9f81
surong <lxdyran@qq.com>
2016-01-02 21:42:03 +0800

    homework css done

--- components/course-main/course-main.js
+++ components/course-main/course-main.js
@@ -1,7 +1,13 @@
-angular.module("courseMain", ['navbar', 'courseAside', 'courseIntroSched', 'courseStudents', 'LocalStorageModule' ])
+angular.module("courseMain", [
+    'navbar',
+    'courseAside',
+    'courseIntroSched',
+    'courseStudents',
+    'homework',
+    'LocalStorageModule' ])
     .controller("courseMain", function($scope, $http, $routeParams, localStorageService) {
         $scope._id = $routeParams._id;
         $scope.param = $routeParams.param;
         $scope.users = localStorageService.get("users");
-        console.log($scope.users.type);
+        console.log($scope.users.courses);
     });
---
+++ components/homework/homework.js
@@ -0,0 +1,25 @@
+angular.module("homework", [])
+    .directive("homework", function() {
+        return {
+            templateUrl: "/components/homework/homework.html",
+            transclude: false,
+            controller: function($scope, $http, $routeParams) {
+                if ($routeParams.param !== "homework")
+                    return false;
+                $http({
+                    method: 'GET',
+                    url: 'API/homework/'+$scope._id
+                }).then(function(res) {
+                    var result = res.data;
+                    if (result.code === 0) {
+                        $scope.homework = result.homework;
+                    } else {
+                        console.error(res);
+                    }
+                }, function(err) {
+                    console.error(err);
+                });
+
+            }
+        }
+    });
