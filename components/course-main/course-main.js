7bcdc9a2cb373644cff908ff63e5f7fb18620c4e
surong <lxdyran@qq.com>
2016-01-13 06:06:22 +0800

    iiii

--- components/course-main/course-main.js
+++ components/course-main/course-main.js
@@ -9,13 +9,13 @@ angular.module("courseMain", [
     .controller("courseMain", function($scope, $http, $routeParams, $location, localStorageService) {
         routeUpdate();
         $scope._id = $routeParams._id;
-        //$scope.$on('$routeUpdate', routeUpdate);
+        $scope.$on('$routeUpdate', routeUpdate);
         $scope.users = localStorageService.get("users");
 
         function routeUpdate() {
             var search = Object.keys($location.search());
-            if (search.length === 0)
-                $location.search('intro');
+            //if (search.length === 0)
+            //    $location.search('intro');
             $scope.param = search[0];
         }
     });
