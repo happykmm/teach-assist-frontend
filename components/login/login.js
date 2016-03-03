ba5e38c1d11c64c348f092c774714b4841c08a43
surong <lxdyran@qq.com>
2016-03-03 11:43:55 +0800

    use closure

--- components/login/login.js
+++ components/login/login.js
@@ -1,44 +1,47 @@
-angular.module('login', ['LocalStorageModule']).
-controller('login', function($scope, $http, $timeout, $location, localStorageService) {
-    $scope.errorMessage = null;
-    $scope.submit = function () {
-        if (!$scope.username || !$scope.password) {
-            console.log("invalid username or password");
-            return false;
+(function() {
+
+    angular.module('login', ['LocalStorageModule']).
+        controller('login', login);
+
+    function login($scope, $http, $timeout, $location, localStorageService) {
+        $scope.errorMessage = null;
+        $scope.submit = function () {
+            if (!$scope.username || !$scope.password) {
+                console.log("invalid username or password");
+                return false;
+            }
+            $http({
+                method: 'POST',
+                url: "API/login",
+                data: {
+                    username: $scope.username,
+                    password: $scope.password
+                }
+            }).then(function (res) {
+                var result = res.data;
+                if (result.code === 0) {
+                    console.log("Login success, token=" + result.token);
+                    localStorageService.set("users", {
+                        number: result.number,
+                        realname: result.realname,
+                        type: result.type,
+                        token: result.token
+                    });
+                    $http.defaults.headers.common["x-access-token"] = result.token;
+                    $location.url('/courses');
+                } else {
+                    $scope.errorMessage = result.desc;
+                    $timeout(function() {
+                        $scope.errorMessage = null;
+                    }, 2000);
+                }
+            }, function (err) {
+                console.error(err);
+            });
         }
-        $http({
-            method: 'POST',
-            url: "API/login",
-            data: {
-                username: $scope.username,
-                password: $scope.password
-            }
-        }).then(function (res) {
-            var result = res.data;
-            if (result.code === 0) {
-                console.log("Login success, token=" + result.token);
-                localStorageService.set("users", {
-                    number: result.number,
-                    realname: result.realname,
-                    type: result.type,
-                    token: result.token
-                });
-                $http.defaults.headers.common["x-access-token"] = result.token;
-                $location.url('/courses');
-            } else {
-                $scope.errorMessage = result.desc;
-                $timeout(function() {
-                    $scope.errorMessage = null;
-                }, 2000);
-            }
-        }, function (err) {
-            console.error(err);
-        });
     }
 
 
-});
-
-
+})();
 
 
