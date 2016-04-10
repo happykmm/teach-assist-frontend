092eb051a51e291c4bbbbfdc2b97c38c0128b53b
surong <lxdyran@qq.com>
2016-04-10 16:44:10 +0800

    DO NOT USE alpha version!!!!!!!

--- components/course-main/course-main.js
+++ components/course-main/course-main.js
@@ -11,17 +11,28 @@
     ]).controller("courseMain", courseMain);
 
 
-    function courseMain($scope, $state, $stateParams, localStorageService) {
+    function courseMain($scope, $state, $stateParams, localStorageService, $rootScope) {
         //以下属性会继承给子控制器
         $scope.course_id = $stateParams.course_id;
         $scope.users = localStorageService.get("users");
-        $scope.state = "";
+        $scope.state = $state.current.name.replace("course.", "");
 
-        routeUpdate();
-        $scope.$on('$locationChangeSuccess', routeUpdate);
-        function routeUpdate() {
-            $scope.state = $state.current.name.replace("course.","");
-        }
+        // console.log($rootScope);
+        // $scope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams, options) {
+        //     alert(1);
+        //     console.log("TOSTATE");
+        //     console.log(toState);
+        //     console.log("TOSTATE");
+        //     $scope.state = toState.name.replace("course.","");
+        // });
+
+        $rootScope.$on('$stateChangeStart',
+            function(event, toState, toParams, fromState, fromParams, options){
+                event.preventDefault();
+                alert(1);
+                // transitionTo() promise will be rejected with
+                // a 'transition prevented' error
+            })
     }
 
 })();
