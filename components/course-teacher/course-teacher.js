ce54041428437c66f7799bac2ea3e7f0ee152040
surong <lxdyran@qq.com>
2016-04-15 15:03:54 +0800

    fix course teacher

--- components/course-teacher/course-teacher.js
+++ components/course-teacher/course-teacher.js
@@ -2,21 +2,16 @@
 
     angular.module('courseTeacher', [
         'ngSanitize'
-    ]).directive('courseTeacher', courseTeacher);
+    ]).controller('courseTeacher', courseTeacher);
 
-    function courseTeacher() {
-        return {
-            templateUrl: '/components/course-teacher/course-teacher.html',
-            transclude: false,
-            controller: function($scope, $http) {
-                $http({
-                    method: 'GET',
-                    url: 'API/courses/'+$scope.course_id+'/teacher'
-                }).then(function(res) {
-                    $scope.intro = res.data.intro;
-                })
-            }
-        }
+    function courseTeacher($scope, $http) {
+        $http({
+            method: 'GET',
+            url: 'API/courses/'+$scope.course_id+'/teacher'
+        }).then(function(res) {
+            $scope.intro = res.data.intro;
+        })
+
     }
 
 })();
