df446db673045f8f6b2cd3bfe127cb724208f9a7
surong <lxdyran@qq.com>
2016-04-16 23:01:09 +0800

    use timeout and bug fixed. amazing but confusing...

--- components/main/main.js
+++ components/main/main.js
@@ -12,13 +12,17 @@
     ]).controller("main", main);
 
     function main($scope, userService, $timeout) {
-        
-        $scope.images = [
-            'http://7xpijn.com1.z0.glb.clouddn.com/carousel01.jpg',
-            'http://7xpijn.com1.z0.glb.clouddn.com/carousel02.jpg',
-            'http://7xpijn.com1.z0.glb.clouddn.com/carousel03.jpg',
-            'http://7xpijn.com1.z0.glb.clouddn.com/carousel04.jpg'
-        ];
+
+        $scope.images = [""];
+
+        $timeout(function() {
+            $scope.images = [
+                'http://7xpijn.com1.z0.glb.clouddn.com/carousel01.jpg',
+                'http://7xpijn.com1.z0.glb.clouddn.com/carousel02.jpg',
+                'http://7xpijn.com1.z0.glb.clouddn.com/carousel03.jpg',
+                'http://7xpijn.com1.z0.glb.clouddn.com/carousel04.jpg'
+            ];
+        }, 0);
 
         $scope.user = userService();
         
