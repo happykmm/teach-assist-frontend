c1c19669ac3afdbab09ae71ef0fd44f028a95960
surong <lxdyran@qq.com>
2016-04-18 16:25:22 +0800

    use public cdn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 (function() {
 
-    var DEBUG = true;
+    var DEBUG = false;
 
     var app = angular.module('teachAssist', [
         'LocalStorageModule',
@@ -31,7 +31,7 @@
                     if (config.url.match(/^API/)) {
                         config.api = true;
                         config.url = config.url.replace(/^API/,
-                            DEBUG ? "http://localhost:9999" :　"https://teachassist.xyz/api");
+                            DEBUG ? "https://localhost:9999" :　"https://teachassist.xyz:9999");
                         config.headers["x-access-token"] = userService().token;
                     }
                     return config || $q.when(config);
@@ -79,29 +79,29 @@
                 {
                     name:"ngCkeditor",
                     files:[
-                        "/bower_components/ckeditor/ckeditor.js",   //already minimized.
+                        "//cdn.bootcss.com/ckeditor/4.5.4/ckeditor.js",   //already minimized.
                         "/bower_components/ng-ckeditor/ng-ckeditor.min.js"
                     ],
                     serie: true
                 },
-                
-                {name:"plupload", files:["/bower_components/plupload/plupload.full.min.js"]},
+
+                {name:"plupload", files:["//cdn.bootcss.com/plupload/2.1.8/plupload.full.min.js"]},
                 {name:"Qiniu", files:["/bower_components/qiniu/src/qiniu.min.js"]},
-                {name:"moment", files:["/bower_components/moment/min/moment.min.js"]},
-                {name:"jQuery", files:["/bower_components/jquery/dist/jquery.min.js"]},
-                {name:"dotdotdot", files:["/bower_components/jQuery.dotdotdot/src/js/jquery.dotdotdot.min.js"]},
-                
+                {name:"moment", files:["//cdn.bootcss.com/moment.js/2.11.1/moment.min.js"]},
+                {name:"jQuery", files:["//cdn.bootcss.com/jquery/2.2.0/jquery.min.js"]},
+                {name:"dotdotdot", files:["//cdn.bootcss.com/jQuery.dotdotdot/1.7.4/jquery.dotdotdot.min.js"]},
+
                 {
-                    name:"angular-carousel", 
+                    name:"angular-carousel",
                     files:[
-                        "/bower_components/angular-carousel/dist/angular-carousel.min.css",
-                        "/bower_components/angular-carousel/dist/angular-carousel.min.js"
+                        "//cdn.bootcss.com/angular-carousel/1.0.1/angular-carousel.min.css",
+                        "//cdn.bootcss.com/angular-carousel/1.0.1/angular-carousel.min.js"
                     ]
                 },
-                {name:"ngTouch", files:["/bower_components/angular-touch/angular-touch.min.js"]},
+                {name:"ngTouch", files:["//cdn.bootcss.com/angular-touch/1.4.8/angular-touch.min.js"]},
                 {name:"focus-if", files:["/bower_components/ng-focus-if/focusIf.min.js"]},
                 {
-                    name:"datePicker", 
+                    name:"datePicker",
                     files:[
                         "/bower_components/angular-datepicker/dist/angular-datepicker.min.css",
                         "/bower_components/angular-datepicker/dist/angular-datepicker.min.js"
@@ -114,113 +114,113 @@
                 {name:"backgroundImage", files:["/directives/background-image.js"]},
 
                 {
-                    name:"usericon", 
+                    name:"usericon",
                     files:[
                         "/components/usericon/usericon.css",
                         "/components/usericon/usericon.js"
                     ]
                 },
                 {
-                    name:"navbar", 
+                    name:"navbar",
                     files:[
                         "/components/navbar/navbar.css",
                         "/components/navbar/navbar.js"
                     ]
                 },
-                
+
                 {
-                    name:"main", 
+                    name:"main",
                     files:[
                         "/components/main/main.css",
                         "/components/main/main.js"
                     ]
                 },
                 {
-                    name:"login", 
+                    name:"login",
                     files:[
                         "/components/login/login.css",
                         "/components/login/login.js"
                     ]
                 },
                 {
-                    name:"courses", 
+                    name:"courses",
                     files:[
                         "/components/courses/courses.css",
                         "/components/courses/courses.js"
                     ]
                 },
                 {
-                    name:"courseMain", 
+                    name:"courseMain",
                     files:[
                         "/components/course-main/course-main.css",
                         "/components/course-main/course-main.js"
                     ]
                 },
                 {
-                    name:"courseAside", 
+                    name:"courseAside",
                     files:[
                         "/components/course-aside/course-aside.css",
                         "/components/course-aside/course-aside.js"
                     ]
                 },
                 {
-                    name:"courseIntroSched", 
+                    name:"courseIntroSched",
                     files:[
                         "/components/course-intro-sched/course-intro-sched.css",
                         "/components/course-intro-sched/course-intro-sched.js"
                     ]
                 },
                 {
-                    name:"courseTeacher", 
+                    name:"courseTeacher",
                     files:[
                         "/components/course-teacher/course-teacher.css",
                         "/components/course-teacher/course-teacher.js"
                     ]
                 },
                 {
-                    name:"courseStudents", 
+                    name:"courseStudents",
                     files:[
                         "/components/course-students/course-students.css",
                         "/components/course-students/course-students.js"
                     ]
                 },
                 {
-                    name:"courseWare", 
+                    name:"courseWare",
                     files:[
                         "/components/course-ware/course-ware.css",
                         "/components/course-ware/course-ware.js"
                     ]
                 },
                 {
-                    name:"homework", 
+                    name:"homework",
                     files:[
                         "/components/homework/homework.css",
                         "/components/homework/homework.js"
                     ]
                 },
                 {
-                    name:"homeworkMain", 
+                    name:"homeworkMain",
                     files:[
                         "/components/homework-main/homework-main.css",
                         "/components/homework-main/homework-main.js"
                     ]
                 },
                 {
-                    name:"postList", 
+                    name:"postList",
                     files:[
                         "/components/post-list/post-list.css",
                         "/components/post-list/post-list.js"
                     ]
                 },
                 {
-                    name:"postDetail", 
+                    name:"postDetail",
                     files:[
                         "/components/post-detail/post-detail.css",
                         "/components/post-detail/post-detail.js"
                     ]
                 },
                 {
-                    name:"self", 
+                    name:"self",
                     files:[
                         "/components/self/self.css",
                         "/components/self/self.js"
